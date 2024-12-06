5be076a1d9b79d1c42b939e80d8ad2c61087c89d
Stuart MacLean <stuart@blackomega.co.uk>
2024-12-06 18:29:17 +0000

    NMake /MP option enabled. Change WinSDK 10.0 kit

--- source/build/cmake/winsdk.cmake
+++ source/build/cmake/winsdk.cmake
@@ -1,10 +1,10 @@
-# Windows 7.1 SDK configuration
+# Windows 10 SDK configuration
 
 if (${CMAKE_GENERATOR} MATCHES "NMake Makefiles")
-	set(WIN_SDK_HOME "D:\\Program Files\\Microsoft SDKs\\Windows\\v7.1" CACHE PATH "Windows SDK 7.1")
+	set(WIN_SDK_HOME "C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.18362.0\\um\\x64" CACHE PATH "Windows SDK 10.0.18362")
 	message("Set ${WIN_SDK_HOME}")
 	
-	set_property(TARGET wbemuuid PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/Lib/WbemUuid.Lib" )
-	set_property(TARGET iphlpapi PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/Lib/IPHlpApi.Lib" )
-	set_property(TARGET winmm PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/Lib/WinMM.Lib" )
+	set_property(TARGET wbemuuid PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/WbemUuid.Lib" )
+	set_property(TARGET iphlpapi PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/IPHlpApi.Lib" )
+	set_property(TARGET winmm PROPERTY IMPORTED_IMPLIB "${WIN_SDK_HOME}/WinMM.Lib" )
 endif (${CMAKE_GENERATOR} MATCHES "NMake Makefiles")
