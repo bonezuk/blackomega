cfcb519158f8b054bebe3eadd31eb42bc315efa0
Stuart MacLean <stuart@blackomega.co.uk>
2025-01-10 19:57:31 +0000

    Remove Help from windows build as using online help

--- source/build/Windows/build_release_2019.bat
+++ source/build/Windows/build_release_2019.bat
@@ -1,6 +1,6 @@
 #"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-set BUILD_NUMBER=670
+set BUILD_NUMBER=672
 
 cd D:\Development
 rmdir /S /Q OmegaBuild
@@ -25,18 +25,12 @@ cd build_versioner
 ninja
 
 cd ..
-.\Build\Omega\bin\versioner.exe .\source\player\Resources\buildInfo.xml %BUILD_NUMBER% .\source\player\Info.plist .\source\player\Info.plist .\source\player\player.rc .\source\installer\Version.nsh ".\source\help\apple\Black Omega.help\Contents\Info.plist" ".\source\help\apple\Black Omega.help\Contents\Info.plist"
-.\Build\Omega\bin\versioner.exe .\source\player\Resources\buildInfo.xml %BUILD_NUMBER% .\source\player\appstore\Info.plist .\source\player\appstore\Info.plist .\source\player\player.rc .\source\installer\Version.nsh ".\source\help\appleStore\Black Omega.help\Contents\Info.plist" ".\source\help\appleStore\Black Omega.help\Contents\Info.plist"
+.\Build\Omega\bin\versioner.exe .\source\player\Resources\buildInfo.xml %BUILD_NUMBER% .\source\player\Info.plist .\source\player\Info.plist .\source\player\player.rc .\source\installer\Version.nsh
 
 mkdir build_cmake_win64
 cmake "-DTIGER_DEBUG_BUILD:BOOL=OFF" "-DQT_HOME=C:\Qt\6.7.2\msvc2019_64" -G "Ninja" -B .\build_cmake_win64 -S .
 cd build_cmake_win64
 ninja
-
-cd ..\source\help
-C:\Qt\6.7.2\msvc2019_64\bin\qhelpgenerator.exe ".\windows\help.qhcp" -o ".\windows\BlackOmega.qhc"
-xcopy /Y ".\windows\BlackOmega.qch" "..\..\Build\Omega\help"
-xcopy /Y ".\windows\BlackOmega.qhc" "..\..\Build\Omega\help"
 
 cd ..\..\Build\Omega\bin
 move "Omega.exe" "Black Omega.exe"
