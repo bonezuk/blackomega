718f1c1a0a1502c0b4389e8dc576881618f8709d
Stuart MacLean <stuart@blackomega.co.uk>
2026-04-17 16:03:22 +0100

    Start plotting out FFT for R2C in DSD upsampling.

--- source/engine/test/integration/FFTRadix2RCTest.cxx
+++ source/engine/test/integration/FFTRadix2RCTest.cxx
@@ -182,3 +182,339 @@ TEST(FFTRadix2RC, FFTRadix2CheckR2CAndC2REquations)
 }
 
 //-------------------------------------------------------------------------------------------
+
+void FFT2_Radix2R2C_A(const tfloat64 x0, const tfloat64 x1, engine::Complex& X0, engine::Complex& X1)
+{
+	X0.R() = x0 + x1;
+	X0.I() = 0.0;
+	X1.R() = x0 - x1;
+	X1.I() = 0.0;
+}
+
+void FFT4_Radix2R2C_A(const tfloat64 x[4], engine::Complex *X)
+{
+	tfloat64 F1[2], F2[2];
+
+	F1[0] = x[0] + x[2];
+	F1[1] = x[0] - x[2];
+	F2[0] = x[1] + x[3];
+	F2[1] = x[1] - x[3];
+
+	X[0].R() = F1[0] + F2[0];
+	X[0].I() = 0.0;
+	X[1].R() = F1[1];
+	X[1].I() = 0.0 - F2[1];
+	X[2].R() = F1[0] - F2[0];
+	X[2].I() = 0.0;
+	X[3].R() = F1[1];
+	X[3].I() = F2[1];
+}
+
+void FFT8_Radix2R2C_A(const tfloat64 x[8], engine::Complex *X)
+{
+	const tfloat64 c_halfSqr = 0.70710678118654752440084436210485;
+	tfloat64 A1[2], A2[2], A3[2], A4[2];
+	engine::Complex F1[4], F2[4];
+
+	A1[0] = x[0] + x[4];
+	A1[1] = x[0] - x[4];
+	A2[0] = x[2] + x[6];
+	A2[1] = x[2] - x[6];
+	A3[0] = x[1] + x[5];
+	A3[1] = x[1] - x[5];
+	A4[0] = x[3] + x[7];
+	A4[1] = x[3] - x[7];
+
+	F1[0].R() = A1[0] + A2[0];
+	F1[0].I() = 0.0;
+	F1[1].R() = A1[1];
+	F1[1].I() = 0.0 - A2[1];
+	F1[2].R() = A1[0] - A2[0];
+	F1[2].I() = 0.0;
+	F1[3].R() = A1[1];
+	F1[3].I() = A2[1];
+
+	F2[0].R() = A3[0] + A4[0];
+	F2[0].I() = 0.0;
+	F2[1].R() = A3[1];
+	F2[1].I() = 0.0 - A4[1];
+	F2[2].R() = A3[0] - A4[0];
+	F2[2].I() = 0.0;
+	F2[3].R() = A3[1];
+	F2[3].I() = A4[1];
+
+	engine::Complex F2_1(c_halfSqr,-c_halfSqr);
+	F2_1 *= F2[1];
+	engine::Complex F2_2(0.0,-1.0);
+	F2_2 *= F2[2];
+	engine::Complex F2_3(-c_halfSqr,-c_halfSqr);
+	F2_3 *= F2[3];
+	
+	X[0] = F1[0] + F2[0];
+	X[1] = F1[1] + F2_1;
+	X[2] = F1[2] + F2_2;
+	X[3] = F1[3] + F2_3;
+	
+	X[4] = F1[0] - F2[0];
+	X[5] = F1[1] - F2_1;
+	X[6] = F1[2] - F2_2;
+	X[7] = F1[3] - F2_3;
+}
+
+void FFT8_Radix2R2C_B(const tfloat64 x[8], engine::Complex* X)
+{
+	const tfloat64 c_halfSqr = 0.70710678118654752440084436210485;
+	tfloat64 A1[2], A2[2], A3[2], A4[2], sA, sB;
+	engine::Complex F1[4], F2[4];
+
+	A1[0] = x[0] + x[4];
+	A1[1] = x[0] - x[4];
+	A2[0] = x[2] + x[6];
+	A2[1] = x[2] - x[6];
+	A3[0] = x[1] + x[5];
+	A3[1] = x[1] - x[5];
+	A4[0] = x[3] + x[7];
+	A4[1] = x[3] - x[7];
+
+	F1[0].R() = A1[0] + A2[0];
+	F1[0].I() = 0.0;
+	F1[1].R() = A1[1];
+	F1[1].I() = 0.0 - A2[1];
+	F1[2].R() = A1[0] - A2[0];
+	F1[2].I() = 0.0;
+	F1[3].R() = A1[1];
+	F1[3].I() = A2[1];
+
+	F2[0].R() = A3[0] + A4[0];
+	F2[0].I() = 0.0;
+	F2[1].R() = A3[1];
+	F2[1].I() = 0.0 - A4[1];
+	F2[2].R() = A3[0] - A4[0];
+	F2[2].I() = 0.0;
+	F2[3].R() = A3[1];
+	F2[3].I() = A4[1];
+
+	// a =  c_halfSqr
+	// b = -c_halfSqr
+	// c = F2[1].R()
+	// d = F2[1].I()
+	engine::Complex F2_1;
+	//F2_1.R() = (c_halfSqr * F2[1].R()) - (-c_halfSqr * F2[1].I()); // (a * c) - (b * d);
+	//F2_1.I() = (-c_halfSqr * F2[1].R()) + (c_halfSqr * F2[1].I()); // (b * c) + (a * d);
+	sA = c_halfSqr * F2[1].R();
+	sB = c_halfSqr * F2[1].I();
+	F2_1.R() = sA + sB;
+	F2_1.I() = sB - sA;
+
+	// a = 0.0
+	// b = -1.0
+	// c = F2[2].R()
+	// d = F2[2].I() = 0.0
+	engine::Complex F2_2;
+	//F2_2.R() = (0.0 * F2[2].R()) - (-1.0 * 0.0); // (a * c) - (b * d);
+	//F2_2.I() = (-1.0 * F2[2].R()) + (0.0 * 0.0);// (b * c) + (a * d);
+	F2_2.R() = 0.0;
+	F2_2.I() = -F2[2].R();
+
+	// a = -c_halfSqr
+	// b = -c_halfSqr
+	// c = F2[3].R()
+	// d = F2[3].I()
+	engine::Complex F2_3;
+	//F2_1.R() = (-c_halfSqr * F2[3].R()) - (-c_halfSqr * F2[3].I()); // (a * c) - (b * d);
+	//F2_1.I() = (-c_halfSqr * F2[3].R()) + (-c_halfSqr * F2[3].I()); // (b * c) + (a * d);
+	sA = -c_halfSqr * F2[3].R();
+	sB = -c_halfSqr * F2[3].I();
+	F2_3.R() = sA - sB;
+	F2_3.I() = sB + sA;
+
+	X[0] = F1[0] + F2[0];
+	X[1] = F1[1] + F2_1;
+	X[2] = F1[2] + F2_2;
+	X[3] = F1[3] + F2_3;
+
+	X[4] = F1[0] - F2[0];
+	X[5] = F1[1] - F2_1;
+	X[6] = F1[2] - F2_2;
+	X[7] = F1[3] - F2_3;
+}
+
+void FFT8_Radix2R2C_C(const tfloat64 x[8], engine::Complex* X)
+{
+	const tfloat64 c_halfSqr = 0.70710678118654752440084436210485;
+	tfloat64 A1[2], A2[2], A3[2], A4[2], sA, sB;
+	engine::Complex F1[4], F2[4];
+
+	A1[0] = x[0] + x[4];
+	A1[1] = x[0] - x[4];
+	A2[0] = x[2] + x[6];
+	A2[1] = x[2] - x[6];
+	A3[0] = x[1] + x[5];
+	A3[1] = x[1] - x[5];
+	A4[0] = x[3] + x[7];
+	A4[1] = x[3] - x[7];
+
+	F1[0].R() = A1[0] + A2[0];
+	F1[0].I() = 0.0;
+	F1[1].R() = A1[1];
+	F1[1].I() = 0.0 - A2[1];
+	F1[2].R() = A1[0] - A2[0];
+	F1[2].I() = 0.0;
+	F1[3].R() = A1[1];
+	F1[3].I() = A2[1];
+
+	F2[0].R() = A3[0] + A4[0];
+	F2[0].I() = 0.0;
+	F2[1].R() = A3[1];
+	F2[1].I() = 0.0 - A4[1];
+	F2[2].R() = A3[0] - A4[0];
+	F2[2].I() = 0.0;
+	F2[3].R() = A3[1];
+	F2[3].I() = A4[1];
+
+	engine::Complex F2_1;
+	sA = c_halfSqr * F2[1].R();
+	sB = c_halfSqr * F2[1].I();
+	F2_1.R() = sA + sB;
+	F2_1.I() = sB - sA;
+
+	engine::Complex F2_3;
+	sA = -c_halfSqr * F2[3].R();
+	sB = -c_halfSqr * F2[3].I();
+	F2_3.R() = sA - sB;
+	F2_3.I() = sB + sA;
+
+	X[0].R() = F1[0].R() + F2[0].R();
+	X[0].I() = F2[0].I();
+
+	X[1].R() = F1[1].R() + F2_1.R();
+	X[1].I() = F1[1].I() + F2_1.I();
+
+	X[2].R() = F1[2].R();
+	X[2].I() = -F2[2].R();
+
+	X[3].R() = F1[3].R() + F2_3.R();
+	X[3].I() = F1[3].I() + F2_3.I();
+
+	X[4].R() = F1[0].R() - F2[0].R();
+	X[4].I() = -F2[0].I();
+
+	X[5].R() = F1[1].R() - F2_1.R();
+	X[5].I() = F1[1].I() - F2_1.I();
+
+	X[6].R() = F1[2].R();
+	X[6].I() = F2[2].R();
+
+	X[7].R() = F1[3].R() - F2_3.R();
+	X[7].I() = F1[3].I() - F2_3.I();
+}
+
+//-------------------------------------------------------------------------------------------
+
+TEST(FFTRadix2RC, FFT2_Radix2R2C_A)
+{
+	const int size = 2;
+	const tfloat64 c_TOLERANCE = 0.00000001;
+	common::Random* rand = common::Random::instance();
+	rand->seed(0);
+
+	tfloat64 *inA = new tfloat64[size];
+	engine::Complex *inB = new engine::Complex[size];
+
+	for(int i = 0; i < size; i++)
+	{
+		inA[i] = rand->randomReal1();
+		inB[i].R() = inA[i];
+		inB[i].I() = 0.0;
+	}
+
+	engine::Complex Xa[2];
+	FFT2_Radix2R2C_A(inA[0], inA[1], Xa[0], Xa[1]);
+
+	engine::Complex *Xb = DFT_N_Full(inB, 2);
+
+	for(int i = 0; i < size; i++)
+	{
+		EXPECT_NEAR(Xa[i].R(), Xb[i].R(), c_TOLERANCE);
+		EXPECT_NEAR(Xa[i].I(), Xb[i].I(), c_TOLERANCE);
+	}
+
+	delete [] Xb;
+	delete [] inB;
+	delete [] inA;
+}
+
+//-------------------------------------------------------------------------------------------
+
+TEST(FFTRadix2RC, FFT4_Radix2R2C_A)
+{
+	const int size = 4;
+	const tfloat64 c_TOLERANCE = 0.00000001;
+	common::Random* rand = common::Random::instance();
+	rand->seed(0);
+
+	tfloat64 *inA = new tfloat64[size];
+	engine::Complex *inB = new engine::Complex[size];
+
+	for(int i = 0; i < size; i++)
+	{
+		inA[i] = rand->randomReal1();
+		inB[i].R() = inA[i];
+		inB[i].I() = 0.0;
+	}
+
+	engine::Complex Xa[4];
+	FFT4_Radix2R2C_A(inA, Xa);
+
+	engine::Complex *Xb = DFT_N_Full(inB, size);
+
+	for(int i = 0; i < size; i++)
+	{
+		EXPECT_NEAR(Xa[i].R(), Xb[i].R(), c_TOLERANCE);
+		EXPECT_NEAR(Xa[i].I(), Xb[i].I(), c_TOLERANCE);
+	}
+
+	delete [] Xb;
+	delete [] inB;
+	delete [] inA;
+}
+
+//-------------------------------------------------------------------------------------------
+
+
+TEST(FFTRadix2RC, FFT8_Radix2R2C_A)
+{
+	const int size = 8;
+	const tfloat64 c_TOLERANCE = 0.00000001;
+	common::Random* rand = common::Random::instance();
+	rand->seed(0);
+
+	tfloat64 *inA = new tfloat64[size];
+	engine::Complex *inB = new engine::Complex[size];
+
+	for(int i = 0; i < size; i++)
+	{
+		inA[i] = rand->randomReal1();
+		inB[i].R() = inA[i];
+		inB[i].I() = 0.0;
+	}
+
+	engine::Complex *Xa = new engine::Complex[size];
+	FFT8_Radix2R2C_C(inA, Xa);
+
+	engine::Complex *Xb = DFT_N_Full(inB, size);
+
+	for(int i = 0; i < size; i++)
+	{
+		EXPECT_NEAR(Xa[i].R(), Xb[i].R(), c_TOLERANCE);
+		EXPECT_NEAR(Xa[i].I(), Xb[i].I(), c_TOLERANCE);
+	}
+
+	delete [] Xa;
+	delete [] Xb;
+
+	delete [] inB;
+	delete [] inA;
+}
+
+//-------------------------------------------------------------------------------------------
